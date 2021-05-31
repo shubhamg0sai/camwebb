bcb8b9137baaf53c2ad84bb56785c204f3600df5
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-31 09:37:40 +0530

    Update camweb.sh

--- camweb.sh
+++ camweb.sh
@@ -108,7 +108,7 @@ $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:
 sleep 8
 fi
 printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... localhost:8080 \e[0m\n"
-fuser -k 3333/tcp > /dev/null 2>&1
+fuser -k 8080/tcp > /dev/null 2>&1
 php -S localhost:8080 > /dev/null 2>&1 &
 sleep 3
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
